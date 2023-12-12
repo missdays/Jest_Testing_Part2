c3cd04b2f1b440b1aa69b6de9807a416bb094b3c
Jen dias <dyeneffer13@hotmail.com>
2023-12-12 10:24:01 +0000

    modify tests and build more functions

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -48,7 +48,7 @@ describe("newGame works correctly", () => {
     test("should clear the player moves array", () => {
         expect(game.playerMoves.length).toBe(0);
     });
-    test("should clear the computer sequence array", () => {
-        expect(game.currentGame.length).toBe(0);
+    test("should add one move to the computer's game array", () => {
+        expect(game.currentGame.length).toBe(1);
     });
 });
